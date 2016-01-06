37a73d1a483bc3583fb923ad8246a84326668527
Marco Schweighauser <marco@sookasa.com>
2016-01-06 12:05:06 +0100

    API endpoint should be CSRF excempt

--- eb_sqs/views.py
+++ eb_sqs/views.py
@@ -1,9 +1,11 @@
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from eb_sqs.worker import Worker
 
 
 @require_http_methods(['POST'])
+@csrf_exempt
 def process_task(request):
     worker = Worker()
 
