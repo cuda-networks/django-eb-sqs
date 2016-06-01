a47e86180b57f0e952f12c27a8203d7df9b6b775
Marco Schweighauser <mschweighauser@barracuda.com>
2016-05-31 17:49:16 -0700

    Overwrite queue_name, max_retries, use_pickle in `delay` function

--- eb_sqs/decorators.py
+++ eb_sqs/decorators.py
@@ -8,16 +8,20 @@ def func_delay_decorator(func, queue_name, max_retries_count, use_pickle):
     # type: (Any, unicode, int, bool) -> (tuple, dict)
     def wrapper(*args, **kwargs):
         # type: (tuple, dict) -> Any
-        queue = queue_name if queue_name else settings.DEFAULT_QUEUE
-        max_retries = max_retries_count if max_retries_count else settings.DEFAULT_MAX_RETRIES
-        pickle = use_pickle if use_pickle else settings.USE_PICKLE
+        queue = _get_kwarg_val(kwargs, 'queue_name', queue_name if queue_name else settings.DEFAULT_QUEUE)
+        max_retries = _get_kwarg_val(kwargs, 'max_retries', max_retries_count if max_retries_count else settings.DEFAULT_MAX_RETRIES)
+        pickle = _get_kwarg_val(kwargs, 'use_pickle', use_pickle if use_pickle else settings.USE_PICKLE)
 
-        execute_inline = kwargs.pop('execute_inline', settings.EXECUTE_INLINE) if kwargs else settings.EXECUTE_INLINE
-        delay = kwargs.pop('delay', settings.DEFAULT_DELAY) if kwargs else settings.DEFAULT_DELAY
-        group_id = kwargs.pop('group_id', None)
+        execute_inline = _get_kwarg_val(kwargs, 'execute_inline',  settings.EXECUTE_INLINE)
+        delay = _get_kwarg_val(kwargs, 'delay',  settings.DEFAULT_DELAY)
+        group_id = _get_kwarg_val(kwargs, 'group_id', None)
 
         worker = WorkerFactory.default().create()
         return worker.delay(group_id, queue, func, args, kwargs, max_retries, pickle, delay, execute_inline)
+
+    def _get_kwarg_val(kwargs, key, default):
+        # type: (dict, unicode, Any) -> Any
+        return kwargs.pop(key, default) if kwargs else default
 
     return wrapper
 
--- eb_sqs/tests/tests_decorators.py
+++ eb_sqs/tests/tests_decorators.py
@@ -16,6 +16,11 @@ def dummy_task(msg):
     if not msg:
         raise Exception('No message')
 
+@task(queue_name='CustomQueue')
+def dummy_task_custom_queue():
+    # type: (unicode) -> None
+    pass
+
 @task()
 def dummy_retry_task(msg):
     # type: (unicode) -> None
@@ -37,6 +42,18 @@ class DecoratorsTest(TestCase):
     def test_delay_decorator(self):
         dummy_task.delay('Hello World!')
         self.worker_mock.delay.assert_called_once()
+
+    def test_delay_custom_queue_decorator(self):
+        dummy_task_custom_queue.delay()
+
+        call_args = self.worker_mock.delay.call_args
+        self.assertTrue('CustomQueue' in call_args[0])
+
+    def test_delay_custom_queue_as_param_decorator(self):
+        dummy_task_custom_queue.delay(queue_name='OtherQueue')
+
+        call_args = self.worker_mock.delay.call_args
+        self.assertTrue('OtherQueue' in call_args[0])
 
     def test_delay_decorator_parameters(self):
         dummy_task.delay('Hello World!', group_id='group-5', delay=5, execute_inline=True)
