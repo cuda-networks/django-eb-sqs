819bf63c511805423de0a1c2a1c6f7f157a6aa8b
Alexey Tsitkin <atsitkin@barracuda.com>
2018-01-30 09:58:42 -0800

    bug fix

--- eb_sqs/aws/sqs_queue_client.py
+++ eb_sqs/aws/sqs_queue_client.py
@@ -45,8 +45,8 @@ class SqsQueueClient(QueueClient):
             queue = self.sqs.create_queue(
                 QueueName=queue_name,
                 Attributes={
-                    'MessageRetentionPeriod': 1209600,
-                    'VisibilityTimeout': 300
+                    'MessageRetentionPeriod': '1209600',
+                    'VisibilityTimeout': '300'
                 }
             )
             self.queue_cache[queue_name] = queue
