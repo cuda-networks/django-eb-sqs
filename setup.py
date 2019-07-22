373cceffdf7786ad23cd75edc46e7487b62fb496
Alexey Tsitkin <atsitkin@barracuda.com>
2019-07-22 14:41:26 -0700

    upgrade requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.md')).read()
 
 setup(
     name='django-eb-sqs',
-    version='1.30',
+    version='1.31',
     package_dir={'eb_sqs': 'eb_sqs'},
     include_package_data=True,
     packages=find_packages(),
@@ -14,9 +14,9 @@ setup(
     long_description=README,
     url='https://github.com/cuda-networks/django-eb-sqs',
     install_requires=[
-        'boto3>=1.4.8',
-        'Django>=1.7',
-        'redis>=2.10',
-        'requests>=2',
+        'boto3>=1.9.86',
+        'Django>=1.10.6',
+        'redis>=2.10.5',
+        'requests>=2.10.0',
     ]
 )
