9ac2d9a413f9876435560d155edb056ab172c419
Alexey Tsitkin <atsitkin@barracuda.com>
2020-01-03 16:21:25 -0800

    make test compatible with py2

--- eb_sqs/tests/aws/tests_aws_queue_client.py
+++ eb_sqs/tests/aws/tests_aws_queue_client.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 import time
 from unittest import TestCase
-from unittest.mock import patch
+from mock import patch
 
 import boto3
 from botocore.exceptions import ClientError
