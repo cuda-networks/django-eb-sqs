d131eb6adcac650de7af2afa7fcae1044a1f2c65
Alexey Tsitkin <alexeyts@users.noreply.github.com>
2020-06-29 16:36:29 -0700

    make default prefix empty, and minor cleanups (#52)
    make default prefix empty, and minor readme & test cleanups

--- eb_sqs/tests/auto_tasks/tests_auto_tasks.py
+++ eb_sqs/tests/auto_tasks/tests_auto_tasks.py
@@ -8,31 +8,31 @@ from eb_sqs.auto_tasks.service import AutoTaskService, _auto_task_wrapper
 
 
 class TestService:
-    _TEST_MOCK = Mock()
-    _MAX_RETRY_NUM = 5
+    TEST_MOCK = None
+    MAX_RETRY_NUM = 5
 
     def __init__(self, auto_task_service=None):
         self._auto_task_service = auto_task_service or AutoTaskService()
 
         self._auto_task_service.register_task(self.task_method)
-        self._auto_task_service.register_task(self.task_retry_method, max_retries=self._MAX_RETRY_NUM)
+        self._auto_task_service.register_task(self.task_retry_method, max_retries=self.MAX_RETRY_NUM)
 
         self._auto_task_service.register_task(self.task_recursive_method)
         self._auto_task_service.register_task(self.task_other_method)
 
     def task_method(self, *args, **kwargs):
-        self._TEST_MOCK.task_method(*args, **kwargs)
+        self.TEST_MOCK.task_method(*args, **kwargs)
 
     def task_retry_method(self, *args, **kwargs):
-        self._TEST_MOCK.task_retry_method(*args, **kwargs)
+        self.TEST_MOCK.task_retry_method(*args, **kwargs)
 
         def max_retry_fun():
-            self._TEST_MOCK.task_max_retry_method(*args, **kwargs)
+            self.TEST_MOCK.task_max_retry_method(*args, **kwargs)
 
         raise RetryableTaskException(Exception('Test'), max_retries_func=max_retry_fun)
 
     def non_task_method(self):
-        self._TEST_MOCK.non_task_method()
+        self.TEST_MOCK.non_task_method()
 
     def task_recursive_method(self, tries=2):
         if tries > 0:
@@ -41,7 +41,7 @@ class TestService:
             self.task_other_method()
 
     def task_other_method(self):
-        self._TEST_MOCK.task_other_method()
+        self.TEST_MOCK.task_other_method()
 
 
 class AutoTasksTest(TestCase):
@@ -53,17 +53,19 @@ class AutoTasksTest(TestCase):
 
         settings.EXECUTE_INLINE = True
 
+        TestService.TEST_MOCK = Mock()
+
     def test_task_method(self):
         self._test_service.task_method(*self._args, **self._kwargs)
 
-        TestService._TEST_MOCK.task_method.assert_called_once_with(*self._args, **self._kwargs)
+        TestService.TEST_MOCK.task_method.assert_called_once_with(*self._args, **self._kwargs)
 
     def test_task_retry_method(self):
         self._test_service.task_retry_method(*self._args, **self._kwargs)
 
-        TestService._TEST_MOCK.task_retry_method.assert_has_calls([call(*self._args, **self._kwargs)] * TestService._MAX_RETRY_NUM)
+        TestService.TEST_MOCK.task_retry_method.assert_has_calls([call(*self._args, **self._kwargs)] * TestService.MAX_RETRY_NUM)
 
-        TestService._TEST_MOCK.task_max_retry_method.assert_called_once_with(*self._args, **self._kwargs)
+        TestService.TEST_MOCK.task_max_retry_method.assert_called_once_with(*self._args, **self._kwargs)
 
     def test_non_task_method(self):
         _auto_task_wrapper.delay(
@@ -73,9 +75,9 @@ class AutoTasksTest(TestCase):
             execute_inline=True
         )
 
-        TestService._TEST_MOCK.non_task_method.assert_not_called()
+        TestService.TEST_MOCK.non_task_method.assert_not_called()
 
     def test_task_recursive_method(self):
         self._test_service.task_recursive_method()
 
-        TestService._TEST_MOCK.task_other_method.assert_called_once_with()
+        TestService.TEST_MOCK.task_other_method.assert_called_once_with()
